4ec686800afa2a5b7c7800428b5fa77dfa7b6c50
A-Brusnitsyn <al.brusnitsyn@yandex.ru>
2026-04-05 17:59:22 +0500

    ci test bd

--- src/test/java/org/brusnitsyn/financetracker/FinanceTrackerApplicationTests.java
+++ src/test/java/org/brusnitsyn/financetracker/FinanceTrackerApplicationTests.java
@@ -3,9 +3,19 @@ package org.brusnitsyn.financetracker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(properties = {
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=PostgreSQL",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.flyway.enabled=false"
+})
 class FinanceTrackerApplicationTests {
 
     @Test
